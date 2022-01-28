3ca7422e629cabf4a5e013734a7674b72384293a
Maciej Wiatr <maciekwiatr17@gmail.com>
2022-01-28 17:12:15 +0100

    Fix overflowing

--- src/components/NotesList/NotesList.jsx
+++ src/components/NotesList/NotesList.jsx
@@ -5,7 +5,7 @@ import styles from "./NotesList.module.scss";
 const NotesList = () => {
 	return (
 		<div className={styles["note-list__container"]}>
-			<NoteItem>Test</NoteItem>
+			<NoteItem>Test długi taki bardzo długi lol jaki długi</NoteItem>
 		</div>
 	);
 };
